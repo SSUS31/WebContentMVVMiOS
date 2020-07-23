6645362f0d4b607b2ce127293dac621e490dc85d
Syed Sadrul Ullah Sahad <sadrul.sahad@bjitgroup.com>
2020-07-23 21:52:33 +0600

    Initial Commit

---
+++ Bongo Disclaimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bongo Disclaimer
+//
+//  Created by Sahad on 23/7/20.
+//  Copyright © 2020 Sahad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
